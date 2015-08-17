133779787561f23da3e63b49f34d9c381945956a
Josh Clyde <joshclyde11@gmail.com>
2015-08-16 21:32:57 -0400

    Initial Commit of Project
    All the files. Hopefully this will work.

---
+++ OfficialProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+
+	public float gravity = 20;
+	public float speed = 9;
+	public float acceleration = 11;
+	public KeyCode jumpKey = KeyCode.Space;
+	public KeyCode jumpKey2 = KeyCode.UpArrow;
+	
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove = new Vector2();
+
+	private PlayerPhysics playerPhysics;
+
+	void Start () {
+		playerPhysics = GetComponent<PlayerPhysics> ();
+	}
+
+	void Update () {
+		targetSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+		currentSpeed = IncrementTowards (currentSpeed, targetSpeed, acceleration);
+
+		if (playerPhysics.grounded) {
+			if (Input.GetKeyDown (jumpKey) || Input.GetKeyDown (jumpKey2) ) {
+				amountToMove.y = 10;
+			} else {
+				amountToMove.y = 0;
+			}
+		} else if (playerPhysics.canAirJump && (Input.GetKeyDown (jumpKey) || Input.GetKeyDown (jumpKey2) ) ) {
+				amountToMove.y = 9;
+				playerPhysics.canAirJump = false;
+
+		}
+
+
+
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move (amountToMove * Time.deltaTime);
+	}
+
+	private float IncrementTowards(float vel, float target, float a){
+		if (vel == target) {//if youre going the inputted speed...
+			return vel;
+		} else {//else, increase or decrease the speed towards the target(inputted speed)
+			float dir = Mathf.Sign (target - vel);
+			vel += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign (target-vel)) ? vel : target;
+		}
+	}
+}
---
+++ OfficialProject/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+
+
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 size;
+	private Vector3 center;
+
+	private float skin = .005f;
+
+	[HideInInspector]
+	public bool grounded;
+
+	[HideInInspector]
+	public bool canAirJump;
+
+	Ray currRay;
+	RaycastHit currHit;
+
+	void Start() {
+		collider = GetComponent<BoxCollider> ();
+		size = collider.size;
+		center = collider.center;
+	}
+
+	public void Move(Vector2 moveAmount) {
+
+		float deltaX = moveAmount.x;
+		float deltaY = moveAmount.y;
+		Vector2 pos = transform.position;
+
+		//print ("X: " + pos.x + " Y: " + pos.y);
+
+		for (int i = 0; i < 3; i++) {
+			float direction = Mathf.Sign(deltaY);
+			float x = (pos.x + center.x - size.x / 2) + size.x / 2 * i; // Left, center, then rightmost
+			float y = pos.y + center.y + size.y / 2 * direction; // Bottom
+
+			currRay = new Ray(new Vector2(x, y), new Vector2(0, direction));
+
+			Debug.DrawRay(currRay.origin, currRay.direction);
+
+			if (Physics.Raycast(currRay, out currHit, Mathf.Abs(deltaY), collisionMask)) {
+				float distance = Vector3.Distance(currRay.origin, currHit.point);
+
+				if (distance > skin) {
+					deltaY = -distance + skin;
+				}
+				else {
+					deltaY = 0;
+				}
+				grounded = true;
+				break;
+			}
+
+			if (grounded) {
+				canAirJump = true;
+			}
+			grounded = false;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			float direction = Mathf.Sign(deltaX);
+			float y = (pos.y + center.y - size.y / 2) + size.y / 2 * i; // Left, center, then rightmost
+			float x = pos.x + center.x + size.x / 2 * direction; // Bottom
+			
+			currRay = new Ray(new Vector2(x, y), new Vector2(direction, 0));
+			
+			Debug.DrawRay(currRay.origin, currRay.direction);
+			
+			if (Physics.Raycast(currRay, out currHit, Mathf.Abs(deltaX), collisionMask)) {
+				float distance = Vector3.Distance(currRay.origin, currHit.point);
+				
+				if (distance > skin) {
+					deltaX = -distance + skin;
+				}
+				else {
+					deltaX = 0;
+				}
+				break;
+			}
+			
+		}
+
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+
+		transform.Translate (finalTransform);
+	}
+}
